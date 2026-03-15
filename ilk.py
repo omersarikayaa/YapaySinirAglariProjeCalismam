ea7be9788ae814d5f06b36a5460d97df6891c7e5
Ömer <omerfaruksarikayaa@outlook.com>
2026-03-15 17:11:09 +0300

    ilk OpenCV çalışması

---
+++ ilk.py
@@ -0,0 +1,13 @@
+import cv2
+
+resim = cv2.imread("istanbulkiz.jpg",0)
+
+cv2.imshow("resim penceresi", resim)
+
+k=cv2.waitKey(0)
+
+if k==ord("q"):
+    print("q tuşuna basıldı resim kaydedildi")
+    cv2.imwrite("kizkulesigri.jpg",resim)    
+
+cv2.destroyAllWindows()
